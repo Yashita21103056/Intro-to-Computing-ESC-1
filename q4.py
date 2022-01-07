6b5025ed4f1a3f4018b9cfb994e3252a1da90888
YASHITA BANSAL <96726018+Yashita21103056@users.noreply.github.com>
2022-01-08 00:12:05 +0530

    Delete q4.py

--- q4.py
+++
@@ -1,8 +0,0 @@
-a=int(input("enter marks of 1st student:"))
-b=int(input("enter marks of 2nd student:"))
-c=int(input("enter marks of 3rd student:"))
-d=int(input("enter marks of 4th student:"))
-e=int(input("enter marks of 5th student:"))
-marks=[a,b,c,d,e]
-marks.sort()
-print(marks)
