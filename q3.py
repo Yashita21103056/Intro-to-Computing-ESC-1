451fc01bc114ed91bac2e283a7e97315b1a4572b
YASHITA BANSAL <96726018+Yashita21103056@users.noreply.github.com>
2022-01-08 00:08:04 +0530

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,7 @@
+SID=int(input("enter your SID:"))
+name=input("enter you name:")
+gender=input("enter your gender('F','M','U'):")
+course=input("enter your course name:")
+CGPA=float(input("enter your CGPA:"))
+student_info=[SID,name,gender,course,CGPA]
+print("student information:",student_info)
