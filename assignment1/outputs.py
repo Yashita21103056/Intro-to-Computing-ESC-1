59a13e59cd8aa2fb27a5c083d77ff90106e3dc1a
YASHITA BANSAL <96726018+Yashita21103056@users.noreply.github.com>
2022-01-09 20:02:32 +0530

    Add files via upload

---
+++ assignment1/outputs.py
@@ -0,0 +1,34 @@
+Python 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========================================================== RESTART: C:\Users\hp\Documents\assignment1\q1.py ==========================================================
+enter a no. a=23
+enter a no. b=46
+enter a no. c=6
+average of the three no.s is 25.0
+>>> 
+=========================================================== RESTART: C:\Users\hp\Documents\assignment1\q2.py ==========================================================
+enter gross income:50000
+enter no. of dependents:10
+your income tax is 2000.0
+>>> 
+=========================================================== RESTART: C:\Users\hp\Documents\assignment1\q3.py ==========================================================
+enter your SID:21103056
+enter you name:Yashita
+enter your gender('F','M','U'):F
+enter your course name:Maths
+enter your CGPA:10
+student information: [21103056, 'Yashita', 'F', 'Maths', 10.0]
+>>> 
+=========================================================== RESTART: C:\Users\hp\Documents\assignment1\q4.py ==========================================================
+enter marks of 1st student:85
+enter marks of 2nd student:76
+enter marks of 3rd student:63
+enter marks of 4th student:92
+enter marks of 5th student:99
+[63, 76, 85, 92, 99]
+>>> 
+=========================================================== RESTART: C:\Users\hp\Documents\assignment1\q5.py ==========================================================
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+>>> 
